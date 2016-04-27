0a0a754438c984ee8e3879149cac450c04a696f3
Herman Brunbäck Larsen <habl500@york.ac.uk>
2016-04-27 12:12:26 +0100

    Lab 3 working
    Streaming is working in external JFrame, prometheus is playing non stop with audio and video visible. User cannot choose video. Sockets close when client window is closed.

--- server/Server.java
+++ server/Server.java
@@ -1,39 +1,67 @@
 package server;
 
+import java.awt.BorderLayout;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
 
+import javax.swing.JFrame;
+
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.headless.HeadlessMediaPlayer;
+import uk.co.caprica.vlcj.runtime.RuntimeUtil;
+import uk.co.caprica.vlcj.test.basic.PlayerControlsPanel;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+
 public class Server {
 	private List<VideoFile> videoList;
 	private ServerSocket serverSocket;
-	private int port =  1238;
+	private String serverAddress = "127.0.0.1";
+	private int listPort =  1337;
+	private int streamPort =  5555;
+	private String options = formatRtpStream(serverAddress, streamPort);
 	private Socket clientSocket;
 	private ObjectOutputStream outputToClient;
 	private Thread socketThread;
+	
 	
 	public static void main(String[] args){
 		new Server(); //Main for testing
 	}
 	
+	private String formatRtpStream(String serverAddress, int serverPort) {
+		StringBuilder sb = new StringBuilder(60);
+		sb.append(":sout=#rtp{dst=");
+		sb.append(serverAddress);
+		sb.append(",port=");
+		sb.append(serverPort);
+		sb.append(",mux=ts}");
+		return sb.toString();
+		}
+
 	public Server(){
 		//Reading and storing the video List from xml file
 		this.videoList = getVideoList();
 		openSocket();
+		setUpMediaStream();
 	}
 	
 	private void openSocket(){
 		socketThread = new Thread("Socket"){
 			public void run(){
 				try {
-					serverSocket = new ServerSocket(port);
+					serverSocket = new ServerSocket(listPort);
 				} catch (IOException e) {
-					System.out.println("Could not open socket on port: " + port + ".");
+					System.out.println("Could not open socket on port: " + listPort + ".");
 					System.exit(-1);
 				}
-				System.out.println("Successfully opened socket on port: "+ port + ", awaiting connection...");
+				System.out.println("Successfully opened socket on port: "+ listPort + ", awaiting connection...");
 				
 				try {
 					//Server stops and awaits client-connection
@@ -47,6 +75,25 @@ public class Server {
 			}
 		};
 		socketThread.start();
+	}
+	
+	private void setUpMediaStream(){
+		this.setUpMediaStream("C:/Users/habl500/JavaFurtherDigital/FDE_mediaplayer/external_archives/VLC/vlc-2.0.1");
+		//this.setUpMediaPlayer("/external_archives/VLC/vlc-2.0.1");
+	}
+	private void setUpMediaStream(String vlcLibraryPath){
+		NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), vlcLibraryPath);
+		Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
+		
+		MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory("server/video_repository/prometheus-featureukFhp.mp4");
+		HeadlessMediaPlayer mediaPlayer = mediaPlayerFactory.newHeadlessMediaPlayer();
+		mediaPlayer.playMedia("server/video_repository/prometheus-featureukFhp.mp4", options, ":no-sout-rtp-sap", ":no-sout-standardsap",":sout-all", ":sout-keep");
+		try {
+			Thread.currentThread().join();
+		} catch (InterruptedException e) {
+			System.out.println("Exception thrown whilst streaming.");
+			e.printStackTrace();
+		}
 	}
 	
 	protected List<VideoFile> getVideoList(){
