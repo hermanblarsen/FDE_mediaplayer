eb482b007b7ce22a4abe11e823e2d98e391223c6
JonathanLR <jlr533@york.ac.uk>
2016-05-07 13:52:14 +0100

    fix tests

--- src/server/ServerTest.java
+++ src/server/ServerTest.java
@@ -2,6 +2,7 @@ package src.server;
 
 import static org.junit.Assert.*;
 
+import java.net.Socket;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,13 +11,14 @@ public class ServerTest {
 	private List<VideoFile> videoList;
 	private Server server;
 	private VideoFile videoFile0;
+	private ClientConnection clientConnection;
 
 	@Before
 	public void setUp() throws Exception {
 		server= new Server();
-		videoList = server.getVideoList();
+		clientConnection = new ClientConnection(new Socket(), 5556, "");
+		videoList = clientConnection.getVideoList();
 		videoFile0 = videoList.get(0);
-		
 	}
 
 	@Test
